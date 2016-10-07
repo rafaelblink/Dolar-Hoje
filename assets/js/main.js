0a1b1673cf60f619e0b1cee8765ecd4baab1f4a6
Rafael Ferreira <rafaelblink@gmail.com>
2016-10-07 20:36:19 -0300

    adding another API

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,33 @@
-var url = 'http://api.promasters.net.br/cotacao/v1/valores?moedas=USD&alt=json';
+var url = 'https://api.fixer.io/latest?base=USD';
 
 var myapp = angular.module('dolarToday', []);
-var value = 0;
 $(".calculate").focus();
 
-myapp.controller('dolarController', ['$scope', function ($scope) {
-  $.getJSON(url, function(json) {
-        $scope.$apply(function(){
-            value = parseFloat(json["valores"]["USD"]["valor"]);
+myapp.controller('dolarController', ['$scope', function($scope) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+
+    xhr.onload = function() {
+        var json = xhr.responseText; // Response
+        json = json.replace(/^[^(]*\(([\S\s]+)\);?$/, '$1'); // Turn JSONP in JSON
+        json = JSON.parse(json); // Parse JSON
+
+        $scope.$apply(function() {
+            var value = parseFloat(json["rates"]["BRL"]);
             $scope.dolar = value.toFixed(2);
         });
-    });
+    };
 
+    xhr.send();
     $scope.result = 0;
-
-    $scope.calculateResult = function(){
-      var valueFloat = 0;
-      valueFloat = parseFloat($scope.inputValue);
-      $scope.result = (valueFloat * $scope.dolar).toFixed(2);
-      if(isNaN($scope.result)){
-        $scope.result = 0;
-      }
+    $scope.calculateResult = function() {
+        var valueInput = $scope.inputValue;
+        var valueInput = valueInput.replace("," , ".");
+        var valueFloat = 0;
+        valueFloat = parseFloat(valueInput);
+        $scope.result = (valueFloat * $scope.dolar).toFixed(2);
+        if (isNaN($scope.result)) {
+            $scope.result = 0;
+        }
     }
 }]);
